00552283e6ebd6891ea3c1c849f1a61c0164bd76
funktasthic <ignacio.avendano@alumnos.ucn.cl>
2025-01-08 08:29:19 -0300

    feat: Add deployed backend url

--- src/app/api/agent.js
+++ src/app/api/agent.js
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { store } from "../store/store";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://backend-taller3-latest-4i2n.onrender.com";
 
 const responseBody = (response) => response.data;
 axios.defaults.withCredentials = true;
